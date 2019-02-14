3396ef6884ffdd2cd26674ff308e5e79a70c0b3c
anuranjanbose <anuranjanbose@gmail.com>
2019-02-14 15:11:07 +0530

    Day7 Q1 source code updated

--- Common/Day7_IntroductionToJavaScript/Ques1/app.js
+++ Common/Day7_IntroductionToJavaScript/Ques1/app.js
@@ -1,13 +1,16 @@
-function calc() {
+	
+		(function() {
 	'use strict';
 	var amount = prompt("Enter amount:");
+	document.getElementById("amount").innerHTML = amount;
+
 
 	var rate = prompt("Enter rate:");
+	document.getElementById("rate").innerHTML = rate;
 
 	var time = prompt("Enter no. of years:");
+	document.getElementById("time").innerHTML = time;
 
 	var interest = (amount*rate*time)/100 ;
 	document.getElementById("interest").innerHTML = interest;
-
-
-}
+}())
