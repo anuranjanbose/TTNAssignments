488a648735973f61db270cf3fadd6d8dbe9aac61
anuranjanbose <anuranjanbose@gmail.com>
2019-03-15 16:39:33 +0530

    Day 10 iOS dded

---
+++ iOS/Day 10 - Size Classes & Auto Layout/AutoLayout/RoundImageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RoundImageViewController.swift
+//  Example
+//
+//  Created by Anuranjan Bose on 13/03/19.
+//  Copyright © 2019 Anuranjan Bose. All rights reserved.
+//
+
+import UIKit
+
+class RoundImageViewController: UIViewController {
+
+    @IBOutlet weak var roundImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        roundImageView.layer.borderWidth = 2.0
+        //roundImageView.layer.borderColor = UIColor.red as! CGColor
+        roundImageView.layer.masksToBounds = false
+        roundImageView.layer.cornerRadius = roundImageView.frame.width/2
+        roundImageView.clipsToBounds = true
+    }
+}
---
+++ iOS/Day 10 - Size Classes & Auto Layout/AutoLayout/WelcomeToAutoLayoutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeToAutoLayoutViewController.swift
+//  Example
+//
+//  Created by Anuranjan Bose on 15/03/19.
+//  Copyright © 2019 Anuranjan Bose. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeToAutoLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
