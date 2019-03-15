1f5dc770b9c1dce037447b328e5c64dc7731c429
anuranjanbose <anuranjanbose@gmail.com>
2019-03-15 16:56:37 +0530

    Day 10 iOS added

---
+++ iOS/Day 10 - Size Classes & Auto Layout/AutoLayout/CalculatorAutoLayoutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorAutoLayoutViewController.swift
+//  Example
+//
+//  Created by Anuranjan Bose on 13/03/19.
+//  Copyright © 2019 Anuranjan Bose. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorAutoLayoutViewController: UIViewController {
+
+    // Calculator using Auto Layout View Controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Calculator Auto Layout View Controller"
+    }
+}
